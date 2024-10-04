51746072436381a23b9be31f6de448faad47f035
Jojosunderland <jojolucysunderland@gmail.com>
2024-10-04 15:18:22 +0100

    downloaded climate data and initial packages begun practical 1 SDM

--- Species distribution modelling/Species_modelling_script.R
+++ Species distribution modelling/Species_modelling_script.R
@@ -21,4 +21,30 @@ bio_curr <-worldclim_country("Switzerland",version="2.1", var='bio', res=10, lon
 bio_fut <- cmip6_world(var = "bio", model = "CNRM-CM6-1-HR", ssp = "245", res = 10,  time = "2041-2060",  lon = c(5.96, 10.49),  lat = c(45.82, 47.81),path=output_dir)[[c(2,5,14)]]
 
 
+# A spatial mask of Switzerland in Swiss coordinates
+bg <- rast('/vsicurl/https://damariszurell.github.io/SDM-Intro/CH_mask.tif')
+
+bio_curr <- terra::project(bio_curr, bg)
+bio_fut <- terra::project(bio_fut, bg)
+#we need to change the projection of our climate data to match that of the bg file.
+
+bio_curr <- terra::resample(bio_curr, bg)
+bio_fut <- terra::resample(bio_fut, bg)
+#we then need to make the resolution equivalent to bg. 
+
+
+bio_curr <- terra::mask(bio_curr, bg)
+bio_fut <- terra::mask(bio_fut, bg)
+#we then need to clip the extent to match an outline of Switzerland
+
+names(bio_curr) <- c('bio_2', 'bio_5', 'bio_14')
+names(bio_fut) <- c('bio_2', 'bio_5', 'bio_14')
+
+# plot the climate variables and their projected change
+plot(bio_curr) # current
+plot(bio_fut) # future
+
+## MODEL FITTING
+# Can you code a binomial GLM with all three predictors fitted as linear and squared terms?
+
 
