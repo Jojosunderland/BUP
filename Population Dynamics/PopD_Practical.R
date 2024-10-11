58e1a7cd807d0504f76b1d51053724c83aecac2f
Jojosunderland <jojolucysunderland@gmail.com>
2024-10-11 11:39:41 +0100

    Added script from BUP W4 Practical 1: Capture-Mark-Recapture

--- Population Dynamics/PopD_Practical.R
+++ Population Dynamics/PopD_Practical.R
@@ -2,4 +2,6 @@
 
 my_packages <- c('dplyr', 'tidyr', 'marked', 'ggplot2', 'R2ucare')
 new_packages <- my_packages[!(my_packages %in% installed.packages()[,'Package'])]
-if(length(new_packages)) install.packages(new_packages)
+if(length(new_packages)) install.packages(new_packages) 
+
+
