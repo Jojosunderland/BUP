0278ae173b904a97472a1ed0eb57719b34739ff7
Jojosunderland <jojolucysunderland@gmail.com>
2024-10-11 13:53:39 +0100

    download and loaded packages

---
+++ Population Dynamics/MPM.Practical.R
@@ -0,0 +1,12 @@
+## Matrix population modelling
+
+# download and load packages
+rm(list=ls(all=TRUE))
+
+my_packages <- c('ggplot2', 'popbio')
+new_packages <- my_packages[!(my_packages %in% installed.packages()[,'Package'])]
+if(length(new_packages)) install.packages(new_packages, dependencies = T)
+
+library(ggplot2)
+library(popbio)
+
